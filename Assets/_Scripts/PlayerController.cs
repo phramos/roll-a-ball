d8da49e27feeb5829e7fc0529c33455169242d65
Pedro Henrique Ramos Souza <phramos@computacao.ufla.br>
2016-07-23 02:25:48 -0300

    Adicionando obstaculos

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerController : MonoBehaviour {
+
+	public Text timeMessage;
+	public Text pickupsLeft;
+
+
+	public float timeSinceReset;
+	public float speed;
+
+	private string minutes;
+	private string seconds;
+
+	private Rigidbody player;
+	private int totalPickups;
+
+	private bool _isPlayerWon;
+	private bool _isGameInProgres;
+
+	void Awake () {
+		
+		player = GetComponent<Rigidbody> ();
+		totalPickups = 25;
+
+		_isPlayerWon = false;
+		_isGameInProgres = true;
+
+		pickupsLeft.text = "Items restantes: " + totalPickups.ToString();
+	
+	}
+
+
+	void Start() {
+		
+		RecalculateTime ();
+
+	}
+
+	void Update () {
+		
+		if (_isGameInProgres) {
+			RecalculateTime (); 
+		}
+
+		if (!_isGameInProgres && Input.GetKey(KeyCode.R)){
+			RestartGame ();
+		}
+	}
+
+	void FixedUpdate() {
+		
+		float moveHorizontal = Input.GetAxis ("Horizontal"); 
+		float moveVertical = Input.GetAxis ("Vertical"); 
+
+		Vector3 moviment = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		player.AddForce (moviment * speed);
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		
+		if (other.gameObject.CompareTag ("Pickup")) {
+
+			other.gameObject.SetActive (false);
+
+			RecalculatePickups (); 
+			
+		} else if (other.gameObject.CompareTag ("Enemy")) {
+			RestartGame ();
+		}
+	}
+
+	void RecalculateTime() {
+
+		if (_isGameInProgres) {
+			minutes = Mathf.Floor (Time.timeSinceLevelLoad / 60).ToString ("00");
+			seconds = ((Time.timeSinceLevelLoad) % 60).ToString ("00");
+
+			timeMessage.text = "Tempo: " + minutes + ":" + seconds;
+		} else {
+			timeMessage.text = "Você coletou tudo em: " + minutes + ":" + seconds;
+		}
+
+	}
+
+	void RecalculatePickups () {
+		if (_isGameInProgres) {
+			totalPickups--;
+			speed++;
+			pickupsLeft.text = "Itens estantes: " + totalPickups.ToString ();
+			if (totalPickups == 0) {
+
+				_isGameInProgres = !_isGameInProgres;
+				_isPlayerWon = true;
+
+			}
+		} 
+			
+	}
+
+	void RestartGame() {
+		timeSinceReset = Time.realtimeSinceStartup;
+		SceneManager.LoadScene (0);
+	}
+
+	float getCurrentTime() {
+		return Time.realtimeSinceStartup - timeSinceReset;
+	}
+
+}
---
+++ Assets/_Scripts/UpAndDown.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpAndDown : MonoBehaviour {
+	private float maxY = 0.829f;
+	private float minY = -3f;
+
+	private float scaleRate= 0.1f;
+
+
+
+	// Update is called once per frame
+	void Update () {
+		ShrinkAndGrow ();
+	}
+
+	void ShrinkAndGrow() {
+	
+		if (transform.position.y < minY) {
+			scaleRate = Mathf.Abs (scaleRate);
+		} else if (transform.position.y > maxY) {
+			scaleRate = -Mathf.Abs (scaleRate);
+		}
+
+		ApplyScaleRate ();
+	
+	}
+
+	void ApplyScaleRate() {
+		//transform.localScale += Vector3.one * scaleRate;
+		//float newY = transform.position.y + scaleRate;
+		Vector3 newPosition = new Vector3(0, scaleRate , 0);
+		transform.position += newPosition;
+	}
+}
